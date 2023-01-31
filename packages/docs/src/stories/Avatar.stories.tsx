90f31e6e94571e137df2e741e2559a27a38f6cc0
jones-bass <jonesbass.tb@gmail.com>
2023-01-31 17:31:32 -0300

    Melhorias Storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/jones-bass.png',
     alt: 'Jones Bass',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
